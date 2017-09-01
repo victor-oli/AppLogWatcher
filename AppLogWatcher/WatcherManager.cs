81ae0317c61bb0660ab2dd04e1a56f271159f10d
João Victor de Oliveira Espinosa <jvespinosa@stone.com.br>
2017-09-01 16:20:17 -0300

    Add project files.

---
+++ AppLogWatcher/WatcherManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+namespace AppLogWatcher
+{
+    public class WatcherManager
+    {
+        private static TelegramBotClient botClient;
+        private static string[] toSendMessages;
+
+        public WatcherManager()
+        {
+            botClient = new TelegramBotClient(ConfigurationManager.AppSettings["TelegramBotToken"]);
+            toSendMessages = ConfigurationManager.AppSettings["ToSendMessages"].Split('/');
+        }
+
+        public void SendMessage(string message, string appName = "N/A")
+        {
+            if (string.IsNullOrWhiteSpace(appName))
+                appName = "N/A";
+
+            toSendMessages = ConfigurationManager.AppSettings["ToSendMessages"].Split('/');
+
+            for (int i = 0; i < toSendMessages.Length; i++)
+            {
+                var temp = botClient.SendTextMessageAsync(toSendMessages[i], $"App: {appName} - {message}", parseMode: ParseMode.Markdown).Result;
+            }
+        }
+
+        public void SendMessage(Exception ex, string appName = "N/A")
+        {
+            SendMessage($"{ex.Message.ToUpper()} -```{ex.StackTrace}```", appName);
+        }
+    }
+}
